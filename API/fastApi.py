dee3b1322925f9da126e9f6210a2da0b8e4478e7
Ben Klingbeil <bmklingbeil@gmail.com>
2024-04-18 10:28:30 -0600

    Did fastapi thing

--- API/fastApi.py
+++ API/fastApi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import mysql.connector
 
 app = FastAPI()
 
@@ -6,3 +7,34 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# Membership management module
+#   GET member profile
+@app.get("/member/{member_id}")
+def read_root():
+    return "a"
+
+#   GET tiers
+@app.get("/member/tiers")
+
+#   PUT payment info
+@app.put("/items/{item_id}")
+
+
+
+# Class scheduling module
+#   GET class list
+#   GET class detail
+#   PUT enroll class
+#   GET currently enrolled classes
+#   GET previously enrolled classes
+
+
+# Progress report module
+#   GET goals
+#   PUT new goal
+#   GET progress graph
+#   GET previously enrolled classes
+#   GET download csv? file
+
